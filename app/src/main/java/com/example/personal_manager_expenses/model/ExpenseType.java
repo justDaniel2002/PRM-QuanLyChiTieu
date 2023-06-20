6ff89be43d1a800e9e5f74f2fdd75566b5860a65
quyenlt <quyenlt@gmail.com>
2023-06-20 14:54:32 +0700

    Tạo model

---
+++ app/src/main/java/com/example/personal_manager_expenses/model/ExpenseType.java
@@ -0,0 +1,42 @@
+package com.example.prm392_personalexpensetracking.model;
+
+import java.util.ArrayList;
+
+public class ExpenseType {
+    private static final ArrayList<ExpenseType> expenseTypeList = new ArrayList<>();
+    private int expenseTypeId;
+    private String expenseTypeTitle;
+
+    public ExpenseType(int expenseTypeId, String expenseTypeTitle) {
+        this.expenseTypeId = expenseTypeId;
+        this.expenseTypeTitle = expenseTypeTitle;
+    }
+
+    public int getExpenseTypeId() {
+        return expenseTypeId;
+    }
+
+    public void setExpenseTypeId(int expenseTypeId) {
+        this.expenseTypeId = expenseTypeId;
+    }
+
+    public String getExpenseTypeTitle() {
+        return expenseTypeTitle;
+    }
+
+    public void setExpenseTypeTitle(String expenseTypeTitle) {
+        this.expenseTypeTitle = expenseTypeTitle;
+    }
+
+    public static void initExpenseType(){
+        expenseTypeList.clear();
+
+        expenseTypeList.add(new ExpenseType(1, "Outcome"));
+        expenseTypeList.add(new ExpenseType(2, "Income"));
+//        expenseTypeList.add(new ExpenseType(3, "Loan / Debt"));
+    }
+
+    public static ArrayList<ExpenseType> getExpenseTypeList(){
+        return expenseTypeList;
+    }
+}
